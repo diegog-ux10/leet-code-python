e480bfec35237f18628a91b6c9b8a61844c04c3b
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-05-15 10:31:02 -0400

    updated

---
+++ product_array_except_self.py
@@ -0,0 +1,16 @@
+def product_except_self(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    output = [1] * (len(nums))
+
+    prefix = 1
+    for i in range(len(nums)):
+        output[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) -1, -1, -1):
+        output[i] *= postfix
+        postfix *= nums[i]
+    return output
