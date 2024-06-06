647d8a7be7b3caec5150d61e4096ddda1ee0d87f
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-06-06 13:41:37 -0400

    valid parentheses

---
+++ validate_parentheses.py
@@ -0,0 +1,15 @@
+def is_valid(s: str) -> bool:
+    stack = []
+    close_to_open = {")": "(", "]":"[", "}": "{"}
+    
+    for c in s:
+        if c in close_to_open:
+            if stack and stack[-1] == close_to_open[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    
+    return True if not stack else False
+    
