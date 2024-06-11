645c90cad103f9d600d8f91c4ca82f11c6aff4b6
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-06-11 16:36:55 -0400

    car fleet

---
+++ car_fleet.py
@@ -0,0 +1,11 @@
+def carFleet(target: int, position, speed) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)]
+        
+        stack = []
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+    
+print(carFleet(target = 10, position = [4,1,0,7], speed = [2,2,1,1]))
